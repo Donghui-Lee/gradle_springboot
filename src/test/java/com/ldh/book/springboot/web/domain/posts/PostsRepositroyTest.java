29636a7e392344b20b0d6c3683cfcb6b8f4f5070
Donghui-Lee <ehdgml3206@naver.com>
2020-11-06 00:57:15 +0900

    머스테치 화면 구현(기본 페이지, 글 등록 화면 구현)

--- src/test/java/com/ldh/book/springboot/web/domain/posts/PostsRepositroyTest.java
+++ src/test/java/com/ldh/book/springboot/web/domain/posts/PostsRepositroyTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -19,11 +20,6 @@ public class PostsRepositroyTest {
 
     @Autowired
     PostsRepository postsRepository;
-
-    @After
-    public void cleanup() {
-        postsRepository.deleteAll();
-    }
 
     @Test
     public void 게시글저장_불러오기() {
@@ -45,4 +41,28 @@ public class PostsRepositroyTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0,0, 0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        // when
+        List<Posts> postsLists = postsRepository.findAll();
+
+        // then
+        Posts posts = postsLists.get(0);
+
+        System.out.println(">>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+    }
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
 }
