fc76ac129ac867dfcccaa49e3c39c8212202a184
dhlee <dhlee@emro.co..kr>
2020-11-09 10:05:31 +0900

    해당 게시글 없을 시 메시지 수정

--- src/main/java/com/ldh/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/ldh/book/springboot/service/posts/PostsService.java
@@ -25,13 +25,13 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
     public PostsResponseDto findById(Long id) {
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id"+ id));
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
         return new PostsResponseDto(entity);
     }
 
