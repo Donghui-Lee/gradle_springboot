8a057c34d300e1673da62bdccfbb61b64d4d6f45
dhlee <dhlee@emro.co..kr>
2020-11-10 09:15:03 +0900

    소셜 로그인(Google) 기능 추가

--- src/main/java/com/ldh/book/springboot/web/IndexController.java
+++ src/main/java/com/ldh/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ldh.book.springboot.web;
 
+import com.ldh.book.springboot.config.auth.dto.SessionUser;
 import com.ldh.book.springboot.domain.posts.PostsRepository;
 import com.ldh.book.springboot.service.posts.PostsService;
 import com.ldh.book.springboot.web.dto.PostsResponseDto;
@@ -9,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
