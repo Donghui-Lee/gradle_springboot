1c784d4734da9a65c0c2a60b51b94f1a7369104d
dhlee <dhlee@emro.co..kr>
2020-11-18 12:38:54 +0900

    세션정보 어노테이션 기반 개선

--- src/main/java/com/ldh/book/springboot/web/IndexController.java
+++ src/main/java/com/ldh/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ldh.book.springboot.web;
 
+import com.ldh.book.springboot.config.auth.LoginUser;
 import com.ldh.book.springboot.config.auth.dto.SessionUser;
 import com.ldh.book.springboot.domain.posts.PostsRepository;
 import com.ldh.book.springboot.service.posts.PostsService;
@@ -17,12 +18,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
